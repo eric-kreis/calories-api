5bfca12541c4e233485cafc07d0e9c9f5f6e4946
Eric Kreis <erickreis793@gmail.com>
2022-11-24 18:51:15 -0300

    First commit :trophy:

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
