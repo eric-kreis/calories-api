c0356e572bcee94f5e4bdc1baac2c0d4f9bb1513
Eric Kreis <erickreis793@gmail.com>
2022-11-30 07:58:36 -0300

    fix: send authorize decorator & pagination dto

--- api/src/shared/dtos/pagination.dto.ts
+++ api/src/shared/dtos/pagination.dto.ts
@@ -1,10 +1,13 @@
+import { Type } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
 export class PaginationDTO {
+  @Type(() => Number)
   @IsNumber()
   @IsOptional()
   page: number = 0;
 
+  @Type(() => Number)
   @IsNumber()
   @IsOptional()
   count: number = 10;
