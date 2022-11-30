447dfe9ba9d2e614659557ddcfaf8ef299f42757
Eric Kreis <erickreis793@gmail.com>
2022-11-29 23:40:50 -0300

    create: user repository

---
+++ api/src/shared/repositories/user/user.repository.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from '@services/prisma.service';
+import { UserRepository } from './user.repository';
+
+@Module({
+  providers: [UserRepository, PrismaService],
+  exports: [UserRepository],
+})
+export class UserModule {}
