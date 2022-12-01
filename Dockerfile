55be55e2a148f7c98c391dfe04230c893ba6cb6e
Eric Kreis <erickreis793@gmail.com>
2022-12-01 11:11:46 -0300

    update: remove api directory

--- Dockerfile
+++ Dockerfile
@@ -32,4 +32,4 @@ COPY --from=builder ./app/dist ./dist
 
 EXPOSE 3000
 
-CMD ["yarn", "start"]
+CMD ["node", "dist/main.js"]
--- src/entry/entry.module.ts
+++ src/entry/entry.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NutrionixAPIServiceModule } from '@services/nutrionix-api/nutrionix-api.service.module';
+import { RoleService } from '@services/role.service';
 import { EntryRepositoryModule } from '@repositories/entries/entry.repository.module';
 import { EntryService } from './entry.service';
 import { EntryController } from './entry.controller';
@@ -7,6 +8,6 @@ import { EntryController } from './entry.controller';
 @Module({
   imports: [NutrionixAPIServiceModule, EntryRepositoryModule],
   controllers: [EntryController],
-  providers: [EntryService],
+  providers: [EntryService, RoleService],
 })
 export class EntryModule {}
