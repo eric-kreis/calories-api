a75095d67096fc1bd31f08cfd9ea827f16db895b
Eric Kreis <erickreis793@gmail.com>
2022-12-01 09:19:01 -0300

    add: entry module to app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { EntryModule } from './entry/entry.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     AuthModule,
+    EntryModule,
   ],
   controllers: [AppController],
   providers: [
